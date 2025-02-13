98be8b2819c59a4da4e87f1b6d9b2a25e6289d77
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2025-02-13 16:13:26 -0300

    i18n funcionando

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const defaultLang = 'es';
+
+export const showDefaultLang = false;
+
+export const languageList = {
+    es: 'Español',
+    en: 'English'
+};
+
+export const labels = {
+    es: {
+        'header.about': 'Acerca de',
+        'header.proyects': 'Proyectos',
+        'header.contact': 'Contacto',
+        'contact.title': 'Póngase en contacto',
+    },
+    en: {
+        'header.about': 'About',
+        'header.proyects': 'Proyects',
+        'header.contact': 'Contact',
+        'contact.title': 'Get in touch',
+    }
+}
